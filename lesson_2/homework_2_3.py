02371d4e78ffe8482cf068786f4a50e01e5c6081
MikeSoloman <Sqasoloman@gmail.com>
2021-12-19 22:46:48 -0500

    Done

--- lesson_2/homework_2_3.py
+++ lesson_2/homework_2_3.py
@@ -1,7 +1,12 @@
-# Write your first program. Enter the temperature right now in Fahrenheit in temperature_fahrenheit variable as
-# a string (e.g. '75') and convert it to Celsius.
-# !important you should save only number to result_temperature. Formula (32°F − 32) × 5/9 = 0°C
+# Write your first program. Enter the temperature right
+# now in Fahrenheit in temperature_fahrenheit variable
+# as a string (e.g. '75') and convert it to Celsius.
+# !important you should save only number to
+# result_temperature. Formula (32°F − 32) × 5/9 = 0°C
 
 # type your code here
-temperature_fahrenheit = None
-result_temperature = None
+temperature_fahrenheit = float(input("Whats the temperature"
+                                     "right now \n"))
+result_temperature = (temperature_fahrenheit - 32) * (5/9)
+
+print(result_temperature)
