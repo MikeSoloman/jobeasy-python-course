f9e8eaae93e71d0c28140a6f9ced6d99c802a6b7
MikeSoloman <Sqasoloman@gmail.com>
2021-12-20 21:39:01 -0500

    Done

--- lesson_3/homework_3_1.py
+++ lesson_3/homework_3_1.py
@@ -1,45 +1,63 @@
 # Enter two numbers. If the first one is greater than the second, save first number in result_1,
 # otherwise save the second number to the result_1 variable.
-
-first_number = None
-second_number = None
-result_1 = None
+from random import randint
+first_number = randint(0, 20)
+second_number = randint(0, 20)
+result_1 = first_number if first_number > second_number else second_number
+print(result_1)
 
 # Enter a random number in number_1 variable. If this number is 20 or
 # higher save “Too high” text to result_2, otherwise save “Thank you”.
-number_1 = None
-result_2 = None
+number_1 = randint(0, 40)
+result_2 = "Too high" if number_1 >= 20 else "Thank you"
+print(result_2)
 
 
 # Enter your first name and last name in first_name and last_name variables. If the length of your first name is under
 # five characters, join them together (without a space) and save it to result_3 variable in upper case. If the length
 # of the first name is five or more characters, save their first name in lower case in result_3 variable.
 
-first_name = None
-last_name = None
-result_3 = None
+first_name = "mike"
+last_name = "soloman"
+result_3 = "".join(first_name + last_name).upper() if len(first_name) < 5 else first_name.lower()
+print(result_3)
 
 
 # Enter a number between 10 and 20 (inclusive) and save number to number_2 variable
 # If they enter a number within this range, save a message “Thank you” to result_4, otherwise a
 # message “Incorrect answer” to result_4.
 
-number_2 = None
-result_4 = None
-
+number_2 = randint(10, 35)
+result_4 = f"Thank you, your number was {number_2}" if 10 <= number_2 <= 20 else f"Incorrect answer {number_2}"
+print(result_4)
 
 # Enter your age. If you are 18 or over, save the message “You can vote” in result_5,
 # if you are aged 17, save the message “You can learn to drive” in result_5 variable,
 # if you are 16, save the message “You can buy a lottery ticket” in result_5,
 # if you are under 16, save the message “You can go Trick-or-Treating” in result_5 variable.
 
-age = None
-result_5 = None
-
+age = randint(12, 24)
+result_5 = ""
+if age:
+    if age >= 18:
+        result_5 = f"Since your age is {age}\nYou can vote!"
+    elif age >= 17:
+        result_5 = f"Since your age is {age}\nYou can learn how to drive!"
+    elif age >= 16:
+        result_5 = f"Since your age is {age}\nYou can buy lottery tickets!"
+    else:
+        result_5 = f"Since your age is {age}\nYou can go to trick and treat!"
+else:
+    result_5 = "Invalid Value"
+print(result_5)
 
 # Enter a number between 1 and 12, save this value to month variable. Find which month is it.
 # (January, February, March, April, May, June, Jule, August, September, October, November, December)
 # Write answer in result_month in lower case
 
-month = None
-result_month = None
+month = randint(1, 12)
+result_month = ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"]
+print(f"Your random num selection was {month} ... which is {result_month[month -1 ]}")
+
+
--- lesson_3/homework_3_2.py
+++ lesson_3/homework_3_2.py
@@ -5,9 +5,22 @@
 # If number is  greater than 15 and less than or equal to 20, save the number raised to the power 3 to result_1
 # Else save the text "Wrong value" to result_1
 
-number = None
+from random import randint
+number = 14
+changed_num = 0
 result_1 = None
-
+if number:
+    if 15 < number <= 20:
+        changed_number = number ** 3
+        print(f"Your original number was {number} and it's {changed_number} now")
+    elif 7 < number <= 15:
+        changed_number = number // 3
+        print(f"Your original number was {number} and it's {changed_number} now")
+    else:
+        changed_number = number * 10
+        print(f"Your original number was {number} and it's {changed_number} now")
+else:
+    pass
 
 # Enter two numbers between 1 and 10, save this values to number_1 variable and number_2 variables.
 # If number_1 and number_2 are greater than 0 and less than or equal to 5 save in the product of their multiplication
@@ -17,18 +30,34 @@ result_1 = None
 # If both numbers are greater than 5 and less than or equal to 10, multiply their sum by 3 and save it to result_2
 # Else save the text "Wrong values, try again" to result_2
 
-number_1 = None
-number_2 = None
+number_1 = 5
+number_2 = 7
 result_2 = None
+
+if 0 < number_1 <= 5 and 0 < number_2<= 5:
+    result_2 = number_1 * number_2
+    print(f"Your num1 was {number_1} and your num2 was {number_2} which resulted in {result_2}")
+elif 5 < number_1 <=10 or 5< number_2 <= 10:
+    result_2 = number_1 + number_2
+    print(f"Your num1 was {number_1} and your num2 was {number_2} which resulted in {result_2}")
+elif 5 < number_1 <= 10 and 5< number_2 <= 10:
+    result_2 = (number_1 + number_2) * 3
+    print(f"Your num1 was {number_1} and your num2 was {number_2} which resulted in {result_2}")
+else:
+    result_2 = "Wrong values"
+    print(result_2)
+
 
 # Enter your first name and save it to first_name variable,
 # then Enter last name and save it to last_name
-# If first_name or last_name are shorter than 6 characters, save a full name (with a space between) to result_3
+# If first_name or last_name are shorter than 6 characters,
+# save a full name (with a space between) to result_3
 # Else save first_name to result_3 as many times as length of last_name value
 
-first_name = None
-last_name = None
-result_3 = None
+first_name = "Mikeeeee"
+last_name = "SUHFUHFHU"
+result_3 = first_name + " " + last_name if len(first_name) < 6 or len(last_name) < 6 else (first_name + " ") * len(last_name)
+print(result_3)
 
 
 # Enter a random number. Save this value to random_number variable
@@ -36,5 +65,20 @@ result_3 = None
 # If a number doesn't meet the first condition, find the remainder of random_value divided by 2.
 # If it is 0, save the text "Even number" to result_4 , else save the message "Odd number"
 
-random_number = None
+random_number = randint(1, 150)
 result_4 = None
+if random_number:
+    if random_number < 10 or random_number > 99:
+        print(f"your number was {random_number}")
+        result_4 = "Please, put in a number between 10 and 99"
+        print(result_4)
+    elif random_number % 2 == 0:
+        print(f"your number was {random_number}")
+        result_4 = "even"
+        print(result_4)
+    elif random_number % 2 != 0:
+        print(f"your number was {random_number}")
+        result_4 = "odd"
+        print(result_4)
+else:
+    pass
