363606165880a62d231a78c67d75442d46a1a9ac
MikeSoloman <Sqasoloman@gmail.com>
2021-12-24 16:13:29 -0500

    updated

--- lesson_4/homework_4_2.py
+++ lesson_4/homework_4_2.py
@@ -1,38 +1,66 @@
 # FOR LOOPS EXERCISES
 
 # Ex. 1
-# Enter your name, save it in name variable and save in result_1 variable your name repeated 3 times (use loops)
+# Enter your name, save it in name variable and save in result_1 variable your name
+# repeated 3 times (use loops)
 
-name_1 = None
-result_1 = None
+name_1 = "Mike"
+result_1 = ""
+
+for _ in range(3):
+    result_1 += name_1
+print(result_1)
 
 # TODO: Here is your code
 
 
 # Ex. 2
-# Modify your previous program so that it will enter your name (save it in variable  name_2) and a number
-# (save in variable number) and then save in result_2 variable your name repeated as many times as number_1 is
+# Modify your previous program so that it will enter your name
+# (save it in variable name_2) and a number
+# (save in variable number) and then save in result_2 variable your name
+# repeated as many times as number_1 is
 # (use loops)
-name_2 = None
-number_1 = None
-result_2 = None
-
+name_3 = "Mike"
+number_1 = 12
+result_2 = ""
+for _ in range(number_1):
+    result_2 += name_3
+print(result_2)
 # TODO: Here is your code
 
 
 # Ex. 3
-# Enter a random string, which includes only digits. Write code which find a sum of digits in this string and save it
+# Enter a random string, which includes only digits.
+# Write code which find a sum of digits in this string and save it
 # into result_3 variable
 
-string_number_1 = None
-result_3 = None
+string_number_1 = "1q0r2r3r4we1qq22q"
+result_3 = 0
+for i in string_number_1:
+    if i.isdigit():
+        result_3 += int(i)
+print(result_3)
 
 # TODO: Here is your code
 
 
 # Ex. 4
-# Create code which sums up all even numbers between 2 and 100 (include 100) and save it in result_4 variable
+# Create code which sums up all even numbers between 2 and 100 (include 100) and
+# save it in result_4 variable
 
-result_4 = None
+result_4 = 0
+for i in range(2, 101):
+    if i % 2 == 0:
+        result_4 += i
+print(result_4)
+
+result_5 = 0
+for i in range(2, 102, 2):
+    result_5 += i
+print(result_5)
+
+print(sum(x for x in range(2, 102, 2)))
+
+print(sum(x for x in range(0, 102) if x % 2 == 0))
 
 # TODO: Here is your code
--- lesson_4/homework_4_3.py
+++ lesson_4/homework_4_3.py
@@ -1,23 +1,66 @@
 # WHILE LOOPS EXERCISES
 
-# Enter a random string in the variable string_1, then enter a character and save it in the variable char_1.
-# Write code, which will count how many times your character is included in your string.
+# Enter a random string in the variable string_1, then enter a character and save
+# it in the variable char_1.
+# Write code, which will count how many times your character is included in your
+# string.
 # Save result to result_1 variable
 
-string_1 = None
-char_1 = None
-result_1 = None
+string_1 = "Samsa Tatli Dedim Sene"
+char_1 = 's'
+result_1 = 0
+counter = 0
+for i in string_1:
+    if char_1 == i.lower():
+        result_1 += 1
+print(result_1)
 
+# or
 
-# Enter a random number and save it in variable number_1. Then create code to multiply all the digits together
+string_1 = "Samsa Tatli Dedim Sene"
+char_1 = 's'
+result_1 = string_1.lower().count('s')
+print(result_1)
+
+# or
+
+resultt_1 = 0
+while counter < len(string_1):
+    if string_1[counter].lower() == char_1:
+        resultt_1 += 1
+    counter += 1
+print(result_1)
+
+# Enter a random number and save it in variable number_1. Then create code to
+# multiply all the digits together
 # and save result in the result_2 variable.
 
-number_1 = None
-result_2 = None
+num_1 = 253
+counter_1 = 0
+my_res = 1
+while counter_1 < len(str(num_1)):
+    my_res *= int(str(num_1)[counter_1])
+    counter_1 += 1
+print(my_res)
 
-
-# Enter a random number and save it in variable number_2. Then create code which will return
+# Enter a random number and save it in variable number_2. Then create code which
+# will return
 # a number with digits of number_1 in reverse order. Save it in result_3 variable
 
-number_2 = None
-result_3 = None
+number_2 = 25723344332
+result_3 = int(str(number_2)[::-1])
+print(result_3)
+print(type(result_3))
+counter_2 = int(len(str(number_2)) -1)
+#or
+
+
+result_3 = ""
+
+while counter_2 > -1 :
+    result_3 += (str(number_2)[counter_2])
+    counter_2 -= 1
+print(result_3)
+
+#or
+
